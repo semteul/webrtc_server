14225fbba0ee243a54232f8b60d631f33a28b31b
Kim Minseok <semteul32@gmail.com>
2025-05-29 23:09:29 +0900

    test

--- public/webrtc/webrtc.js
+++ public/webrtc/webrtc.js
@@ -9,7 +9,9 @@ let dataChannel;
 let isOfferer = false;
 
 function generateUUID() {
-  return crypto.randomUUID(); // 대부분의 최신 브라우저 지원
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
 }
 
 const servers = {
